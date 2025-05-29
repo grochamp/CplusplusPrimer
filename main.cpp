1e547a205930dcd4779b410ca4186217b949ed9a
HullBreaker <grochamp5112@gmail.com>
2025-05-29 15:46:53 +0900

    day2

--- main.cpp
+++ main.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
-#include<climits> //정수타입 범위 안전하게 알수있음
 
 using namespace std;
 
-int main() {
-//";" 세미콜론은 종결자 역할을 한다
-//endl을 써야 줄바꿈이된다
-//cout : 출력시켜주세요
-    cout << "HELLO World" << endl;
+int main(){
 
+    //char : 작은 문자형
+    //null 문자를 만나지 않으면 출력이 계속된다. null을 명시해줘야한다.
+    // 큰따옴표 : null이 명시적으로 포함된 의미. 작은따옴표는 아니다. char형은 한글자만 취급하기때문에 null을 사용하는 큰따옴표를 쓸 수 없다.
+
+    char a = 'b';
+    cout << a << endl;
     
+    //bool은 거짓 혹은 참   
+    //거짓은 0, 참은 1
+    //그 외 값을 넣으면 1이 된다.
+    bool b = 1;
+    bool c = 0;
+    bool d = 2;  //1이 나온다다
+    cout << b << c << d<< endl;
 
-    //변수: 숫자로 시작 불가. 공백사용 불가, C++의 키워드 사용 불가가
-    int a;
-    a = 7;
-    int b = 3; //선언하고 바로 대입 : 초기화
-    cout <<"a의 값은 " << a <<", b의 값은 " << b << " 입니다."<< endl;
+    //상수 선언. 바로 값을 대입해줘야함
+    const float PIE = 3.14;
+
+    //강제 데이터 변환
+    //방식 : static_cast<typename>(데이터명)
+    char ch = 'M';
+    cout << static_cast<int>(ch) << endl;
 
 
-    //정수형 : int, short, long 등등 있음
-    //unsigned => 음수를 사용하지 않겠다는 선언. 양수를 두배로 쓸수있다.
-    //실수형 : float
-    float c = 6.21;
-    cout << c << endl;
+
+
+
     return 0;
 
-}
 
+}
