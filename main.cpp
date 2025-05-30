7459b96e24ab0e2fc8383d4f098b32495af8483c
HullBreaker <grochamp5112@gmail.com>
2025-05-30 16:33:12 +0900

    구조체체

--- main.cpp
+++ main.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    //배열 : 같은 데이터형의 집함
-    //typeName arrayName[arraySize]
-    //int xyz[3] int데이터형의 3개의 항목을 가지는 xyz라는 이름의 배열
+
+    //구조체 : 여러 데이터형이 허영되는 데이터형의 집합, 배열은 같은 데이터형의 집합
+    struct MyStruct{
+        string nmae;
+        string position;
+        float height;
+        float weight;
+
+
+    };
+    // B;
+    MyStruct C[2] = {
+        {"A", "B", 11, 13},
+        {"B", "D", 3, 0}
+    };
+    cout << C[1].height << endl;
+
+    MyStruct A = {
+        "박지성", "미드필더", 177, 77
+    };
+    cout << A.position << endl;
     
-    short month[12] = {1, 2, 3};
-    cout << month[0] << endl;
-
-    //12개 원소를 전부 지정하지 않으면, 지정하지 않은 값들은 0이된다. month[3] = 0
-    //배열을 다른 배열에 통째로 대입 불가
-    //따로 arraysize를 designate 안하면 선언한 인수 갯수로 arraysize가 자동으로 정해진다. 
-
-    char a[6] = { 'h', 'e', 'l', 'l', 'o', '\0'};   //5개 인수에 널문자를 추가해서 6개로 설정
-    cout << a << endl;
-
-    char b[] = "hello";  //비워두면 알아서 hello의 갯수대로 arraysize를 정함. 쌍따옴표로 널문자를 대신함.
-    cout << b << endl;
-
-
-    char name1[40];
-    // cin>> name1;
-    // cout << name1 <<endl;
-    cin.getline(name1, sizeof(name1)); //공백도 가능 
-    // cout << name1 << endl;
+    // B ={"","공격수"};
     
-    //string 장점 : arraysize 할당 안해도됨
-    string str1 = string(name1);
-    cout << str1 << endl;
 
+    // cout << B.position<<endl;
+    // cout << B.height<<endl;
 
 
     return 0;
