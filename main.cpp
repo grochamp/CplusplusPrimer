fea5e11f6ec90c43729d96c37af22bddfb9a96c1
HullBreaker <grochamp5112@gmail.com>
2025-05-30 16:12:27 +0900

    string

--- main.cpp
+++ main.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
-
-    //char : 작은 문자형
-    //null 문자를 만나지 않으면 출력이 계속된다. null을 명시해줘야한다.
-    // 큰따옴표 : null이 명시적으로 포함된 의미. 작은따옴표는 아니다. char형은 한글자만 취급하기때문에 null을 사용하는 큰따옴표를 쓸 수 없다.
-
-    char a = 'b';
-    cout << a << endl;
+int main()
+{
+    //배열 : 같은 데이터형의 집함
+    //typeName arrayName[arraySize]
+    //int xyz[3] int데이터형의 3개의 항목을 가지는 xyz라는 이름의 배열
     
-    //bool은 거짓 혹은 참   
-    //거짓은 0, 참은 1
-    //그 외 값을 넣으면 1이 된다.
-    bool b = 1;
-    bool c = 0;
-    bool d = 2;  //1이 나온다다
-    cout << b << c << d<< endl;
+    short month[12] = {1, 2, 3};
+    cout << month[0] << endl;
 
-    //상수 선언. 바로 값을 대입해줘야함
-    const float PIE = 3.14;
+    //12개 원소를 전부 지정하지 않으면, 지정하지 않은 값들은 0이된다. month[3] = 0
+    //배열을 다른 배열에 통째로 대입 불가
+    //따로 arraysize를 designate 안하면 선언한 인수 갯수로 arraysize가 자동으로 정해진다. 
 
-    //강제 데이터 변환
-    //방식 : static_cast<typename>(데이터명)
-    char ch = 'M';
-    cout << static_cast<int>(ch) << endl;
+    char a[6] = { 'h', 'e', 'l', 'l', 'o', '\0'};   //5개 인수에 널문자를 추가해서 6개로 설정
+    cout << a << endl;
+
+    char b[] = "hello";  //비워두면 알아서 hello의 갯수대로 arraysize를 정함. 쌍따옴표로 널문자를 대신함.
+    cout << b << endl;
 
 
+    char name1[40];
+    // cin>> name1;
+    // cout << name1 <<endl;
+    cin.getline(name1, sizeof(name1)); //공백도 가능 
+    // cout << name1 << endl;
+    
+    //string 장점 : arraysize 할당 안해도됨
+    string str1 = string(name1);
+    cout << str1 << endl;
 
 
 
     return 0;
-
-
 }
