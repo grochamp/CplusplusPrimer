6d0a424cef366378ce1ecc368a627aa7a519111c
HullBreaker <grochamp5112@gmail.com>
2025-06-01 01:42:45 +0900

    함수와 배열(포인터)

--- main.cpp
+++ main.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
 using namespace std;
 
-//함수 원형에는 함수명, 매게변수만 있으면 된다
-void multiply(int x, int y);
+const int SIZE = 9;//9개로 이루어짐
 
-void welcome(int, int);//리턴값 없는 함수
-float number(int z);//리턴값 있는 함수
+int sumArray(int[], int);//배열과 x번쨰 배열
+//void가 아닌 typename로 선언했으니까 return값있음
+
 
 int main(){
-//리턴값이 없는 함수 : void로 시작
-//있는 함수 : typeName 알려야함
 
+    int arr[SIZE] ={ 1, 3, 5, 7, 9, 11 ,13 ,15, 17}; //배열 선언
+    int sum = sumArray(arr, SIZE);//배열 형식
 
-
-int a, b;
-cout << "a값 ";
-cin >> a;
-cout << "b값 ";
-cin >> b;
-multiply(a, b);
-
-
-
-
-    return 0;
-}
-void multiply(int x, int y) {
-    cout << x*y << endl;
+    cout << "총 합은 " << sum << endl;
+    cout << arr << endl; //arr = arr[0] 포인터터
+    cout << &arr[2] << endl;
     
+    return 0;
+
+
+}
+int sumArray(int arr[], int n){//함수구조
+int total = 0;
+for (int i = 0; i < n; i++)//함수0번째수부터 끝까지
+total += arr[i];//모든 항목 더하기
+return total;
+
+
 }
