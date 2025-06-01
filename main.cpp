5519427529b805ba51ebe91d88e697c0600e0a80
HullBreaker <grochamp5112@gmail.com>
2025-06-01 10:33:03 +0900

    함수 구조체

--- main.cpp
+++ main.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 using namespace std;
 
-const int SIZE = 9;//9개로 이루어짐
-
-int sumArray(int[], int);//배열과 x번쨰 배열
-//void가 아닌 typename로 선언했으니까 return값있음
 
 
-int main(){
+struct Time
+{
 
-    int arr[SIZE] ={ 1, 3, 5, 7, 9, 11 ,13 ,15, 17}; //배열 선언
-    int sum = sumArray(arr, SIZE);//배열 형식
+    int hours;
+    int mins;
+};
 
-    cout << "총 합은 " << sum << endl;
-    cout << arr << endl; //arr = arr[0] 포인터터
-    cout << &arr[2] << endl;
-    
-    return 0;
+const int minsPerHr = 60;
+Time sum(Time*, Time*);
+void showTime(Time);
+
+int main()
+{
+//함수는 원본이 아닌 복사본을 대상으로 작업한다.
+
+Time day1 = {1, 45};
+Time day2 = {2, 17};
+Time total = sum(&day1, &day2);   //&: 주소연산자
+
+cout << "소요시간 : ";
+showTime(total);
+
+return 0;
+}
+
+Time sum(Time* t1, Time* t2){
+    Time total;
+
+    total.mins = (t1->mins + t2->mins) %minsPerHr;  //포인터에서 접근할때는 ->사용
+    total.hours = t1->hours + t2->hours +
+    (t1->mins + t2->mins) / minsPerHr;
+
+    return total;
+
 
 
 }
-int sumArray(int arr[], int n){//함수구조
-int total = 0;
-for (int i = 0; i < n; i++)//함수0번째수부터 끝까지
-total += arr[i];//모든 항목 더하기
-return total;
-
-
+void showTime(Time t1) {
+    cout << t1.hours << "시간 " << t1.mins << "분" << endl;
 }
