cdd57b4df85c5e1b9edca4a712191975e6dff5ce
HullBreaker <grochamp5112@gmail.com>
2025-06-02 19:52:59 +0900

    1.13

--- main.cpp
+++ main.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 int main(){
 
-    int a, b= 0;
-    cin >> a >> b;
-    while(a<b-1){
-        
-        cout << a+1 << endl;
-        ++a;
-    }
 
-
-        return 0;
-
+    int sum = 0;
+    int number = 0;
+    for (number=0; number<=10; ++number)
+    
+        /* code */sum +=number;
+    
+    cout << sum << endl;
+    
+return 0;
 }
