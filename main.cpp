4effa8eabe64a4f5744edbca1526b30d6f29e191
HullBreaker <grochamp5112@gmail.com>
2025-06-02 20:56:22 +0900

    1.4.4 if 문문

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
-    int b = 0;
-    int sum = 0;
-while (cin >> b){
-    
-    sum +=b;
-    cout << sum << endl;
+
+int currVal = 0, val = 0;
+if (cin >> currVal){ //입력이 성공하면 
+    int cnt = 1;
+    while (cin >> val){ // 입력이 성공하면
+        if (val == currVal)
+        ++cnt; //카운트 늘리기
+        else {
+            cout << currVal << " occurs "
+            << cnt << "times" << endl;
+            currVal = val;
+            cnt = 1;
+//다른 순간 출력
+        }
+    }
+    cout << currVal << " occurs " 
+    << cnt << " times" << endl;
+    // 마지막 값을 출력하기위해 while문 이후에 출력
+
+}
 
 
 
 
 
-    
-}return 0;}
+    return 0;
+}
