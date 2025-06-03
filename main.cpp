c30f347746174ccc6241960e7f4dec14c1aecf02
HullBreaker <grochamp5112@gmail.com>
2025-06-03 17:45:44 +0900

    3.4

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
 int main(){
-string word;
-while (cin >> word)  //이렇게만 해도 알아서 공백인지하면 각각의 단어로 인지함
-cout << word << endl;
+
+    string a, b;
+    cin >> a >> b;
+    if (a.length() == b.length()){
+        cout << "a=b" << endl;
+
+    }
+    else if (a.length() > b.length()){
+        cout << "a 가 더 큼" << endl;
+    }
+    else{
+        cout << "b 가 더 큼" << endl;
+    }
 
     return 0;
+
 }
