11f3418be33f0d539ce487f80e91ce5b54204c7c
HullBreaker <grochamp5112@gmail.com>
2025-06-03 17:36:26 +0900

    3.2

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 using namespace std;
-struct Sales_data{
-
-
-    string bookNo; //값 명시 안했으니 빈 문자열
-    unsigned units_sold = 0; //0으로 명시
-    double revenue = 0.0;
-
-};
 int main(){
-
-
+string word;
+while (cin >> word)  //이렇게만 해도 알아서 공백인지하면 각각의 단어로 인지함
+cout << word << endl;
 
     return 0;
 }
