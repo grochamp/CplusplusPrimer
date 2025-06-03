a27f305946bf85cc3362c5e9742506d2273f5ec7
HullBreaker <grochamp5112@gmail.com>
2025-06-03 17:51:30 +0900

    3.5

--- main.cpp
+++ main.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-
-    string a, b;
-    cin >> a >> b;
-    if (a.length() == b.length()){
-        cout << "a=b" << endl;
-
-    }
-    else if (a.length() > b.length()){
-        cout << "a 가 더 큼" << endl;
-    }
-    else{
-        cout << "b 가 더 큼" << endl;
-    }
+string word;
+while (cin >> word)  
+cout << word << " " 
 
     return 0;
-
 }
