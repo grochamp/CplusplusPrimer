c209e4e3e906b79a048d8e812fd3a6c23fd08894
HullBreaker <grochamp5112@gmail.com>
2025-06-04 11:05:07 +0900

    3.31

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int main(){
-
-    vector<int> number(10);
-    for (int a = 0; a< 10; ++a)
-    { cin >> number[a];}
-    for (auto& n : number){
-        n = 10*n;
-         cout << n << " ";
-    } 
-   return 0;
-    
+    int number[10];
+    int a = 0;
+    for (a; a<10; ++a)
+    {
+        number[a] = a;
+        cout << number[a] << " ";
+    }
+    return 0;
 }
