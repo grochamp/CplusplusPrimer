1897ee45d31a5e0e7d2c1c001a81b0c981d1a8df
HullBreaker <grochamp5112@gmail.com>
2025-06-04 10:35:43 +0900

    3.23

--- main.cpp
+++ main.cpp
@@ -1,29 +1,15 @@
-#include <string>
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <cctype>
-
 using namespace std;
 int main(){
-string word;
-vector<string> words;
-int a = 0;
-int count = 0;
-while(cin >> word){
-transform(word.begin(), word.end(), word.begin(), ::toupper); //stl써서  word전체를 toupper 적용해서 대문자로바꿈
-words.push_back(word);
 
-}
-for(a; a<words.size(); ++a){
-    cout << words[a] << " ";
-    count++;
-
-    if (count % 8 == 0)
-
-{
-    /* code */cout << endl;
-}
-}
-    return 0;
+    vector<int> number(10);
+    for (int a = 0; a< 10; ++a)
+    { cin >> number[a];}
+    for (auto& n : number){
+        n = 10*n;
+         cout << n << " ";
+    } 
+   return 0;
+    
 }
