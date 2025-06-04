45229336780489d041d003b9007d62fc6c8543b9
HullBreaker <grochamp5112@gmail.com>
2025-06-04 20:15:17 +0900

    4.6

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
+int a = 0;
 int main(){
 
-int ia[3][4];
-    for (int (*p)[4] = ia; p != ia +3; ++p){
-        for (int *q = *p; q != *p + 4; ++q){
-            cout << *q << " ";
+ 
+cin >> a;
+if (a % 2 == 0){
+    cout << "짝수" << endl;
+    
 }
-    cout << endl;
+else{
+        cout << "홀수" << endl;
+        
     }
-
     return 0;
 }
