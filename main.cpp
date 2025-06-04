80899c80cc70936edc5e23f0cac3c4315ae30d0b
HullBreaker <grochamp5112@gmail.com>
2025-06-04 17:25:49 +0900

    3.36

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+int number[] = {1, 3 ,5 ,7};
+int nuumber[] = {1, 3, 5 ,9};
 int main(){
-    int number[10];
-    int a = 0;
-    for (a; a<10; ++a)
+    if (equal(begin(number),end(number),begin(nuumber)))
     {
-        number[a] = a;
-        cout << number[a] << " ";
+        /* code */cout << "같음" << endl;
     }
+    else{
+        cout << "다름" << endl;
+    }
+    
     return 0;
 }
