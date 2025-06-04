6f95c08bbedadfd7d21f21e18cb66a13df7eddbf
HullBreaker <grochamp5112@gmail.com>
2025-06-04 23:46:02 +0900

    4.36 명시적변환사용용

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-
-    cout << "char의 크기는 " << sizeof(char) << " 이다" << endl;
-    cout << "int의 크기는 " << sizeof(int) << " 이다" << endl;
-    cout << "string의 크기는 " << sizeof(string) << " 이다" << endl; //32
-    cout << "bool의 크기는 " << sizeof(bool) << " 이다" << endl;
-    cout << "float의 크기는 " << sizeof(float) << " 이다" << endl;
-    
+int i = 5;
+double d = 3.14;
+i *=static_cast<int>(d);
+cout << i << endl;
     return 0;
 }
