27cb39d6bf891da646f2e431ee6e1f7644f5731b
HullBreaker <grochamp5112@gmail.com>
2025-06-05 01:16:49 +0900

    true 값 확인인

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-int i = 5;
-double d = 3.14;
-i *=static_cast<int>(d);
-cout << i << endl;
-    return 0;
+    cout << true + 1 << endl;
+    return 0; 
 }
