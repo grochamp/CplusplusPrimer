02a48fcae74ab42545e54fc1f99a1dfe3b59e3e3
HullBreaker <grochamp5112@gmail.com>
2025-06-05 21:13:27 +0900

    39 과제제

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
-#include <iostream>
 #include <bitset>
+#include <iostream>
 int main(){
-using namespace std;
+    using namespace std;
+      
+    int a = 0b0101;
+    int b = 0b1100;
+    cout << bitset<4> (a & b) << endl;
+    cout << bitset<4> (a | b) << endl;
+    cout << bitset<4> (a ^ b) << endl;
 
-    unsigned int a = 3;
-    unsigned int x = 0b1100;//2진수 표현볍 : 앞에 0b붙이기
-    unsigned int y = 0b0110;
-
-    cout << bitset <4>(a) << endl;//bitset : 2진수 자릿수
-
-    unsigned int b = a << 2; //왼쪽으로 x을 옮기고 나머지 오른쪽은 비움(2의x승)
-    cout << bitset<4>(b) << endl;
-
-    cout << bitset<4>(x & y) << endl; //and : 둘다 1이면 1
-    cout << bitset<4>(x | y) << endl; //or : 하나만 1이면 1
-    cout << bitset<4>(x ^ y) << endl; //두 수 일치 안하는부분 : 1, 두수 같으면 :0
-
-
+    
+    
+    
     return 0;
 }
