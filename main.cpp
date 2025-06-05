8d4420a1a386d388e6bd27f4d1ea3368d688d33d
HullBreaker <grochamp5112@gmail.com>
2025-06-06 01:44:17 +0900

    44 auto

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
 
-namespace a{
-    int my_var =10;
-}
-
-namespace b{
-    int my_var = 20;
+int adding (int x, int y){
+    return x + y;
 }
 
 int main(){
-    {using namespace b;
-        cout << my_var << endl;
-    }
-    {using namespace a;
-        cout << my_var << endl;
-    }
+
+auto a = 123; //자료형 스스로 판단함
+auto b = 3.14;
+auto result = adding(1, 2);
+cout << result << endl;//auto를 parameter type에는 사용 불가
+
+
+
     return 0;
 }
