e570762c82f439efd9ec7e590488c9fbdefb86cb
HullBreaker <grochamp5112@gmail.com>
2025-06-05 22:06:48 +0900

    40 bitmask

--- main.cpp
+++ main.cpp
@@ -2,44 +2,24 @@
 #include <bitset>
 int main(){
     using namespace std;
+    const unsigned int red_mask = 0xFF0000;
+    const unsigned int green_mask = 0x00FF00;
+    const unsigned int blue_mask = 0x0000FF;
+    cout << bitset<32>(red_mask) << endl;
+    cout << bitset<32>(green_mask) << endl;
+    cout << bitset<32>(blue_mask) << endl;
+    
 
+    unsigned int pixel_color = 0xDAA520;
 
-    const unsigned char opt0 = 1 << 0;
-    const unsigned char opt1 = 1 << 1; //1자리 밀음
-    const unsigned char opt2 = 1 << 2; //2자리 밀음
+    cout << bitset<32>(pixel_color) << endl;
+    unsigned char red = (pixel_color & red_mask) >> 16;
+    unsigned char green = (pixel_color & green_mask) >> 8;//8칸 오른쪽으로 밀어주기
+    unsigned char blue =  pixel_color & blue_mask;
 
-    cout << bitset<8>(opt0) << endl;
-    cout << bitset<8>(opt1) << endl;
-    cout << bitset<8>(opt2) << endl;
-    unsigned char items_flag =0;
-    cout << bitset<8>(items_flag) << endl;//2진수를 이용해 8칸을 활용할수있다
+    cout << "red" << bitset<8>(red) << " " << int(red) << endl;
+    cout << "green" << bitset<8>(green) << " " << int(green) << endl;
+    cout << "blue" << bitset<8>(blue) << " " << int(blue) << endl;
+    
 
-    //아이템 획득표시
-    items_flag = opt0;
-    cout << "Get Item0" << " " << bitset<8>(items_flag) << endl;
-    items_flag |= opt2;// |= 값 덮어씌우기가 아닌 갱신
-    cout << "Get Item2" << " " << bitset<8>(items_flag) << endl;
-
-    items_flag &= ~opt2;//잃어버림(덮어씌우기 아니라 갱신임)
-    cout << "Lost Item2" << " " << bitset<8>(items_flag) << endl;
-//확인
-    if(items_flag & opt1) {
-        cout << "has item1" << endl;
-    }
-    else{cout << "no item1" << endl;}
-    if(items_flag & opt0) {
-        cout << "has item0" << endl;
-    }
-    else{cout << "no item0" << endl;}
-
-//obtain item 2, 3(동시에)
-items_flag |= (opt2 | opt1);
-
-cout << bitset<8>(items_flag) << endl;
-cout << "Items 2, 3 obtained" << bitset<8>(items_flag) << endl;
-
-
-
-
-    return 0;
-}
+return 0;}
