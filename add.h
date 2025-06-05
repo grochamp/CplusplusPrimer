373d527be3f28dbe3e715313ec219dedecc2125a
HullBreaker <grochamp5112@gmail.com>
2025-06-05 13:09:42 +0900

    21 namepsace쓰기기

--- add.h
+++ add.h
@@ -1,1 +1,2 @@
+#pragma once//헤더가드
 int add (int a, int b);
--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "add.h"
 using namespace std;
-
+int a, b = 0;
+namespace special1{
+    int sppecial1(int a, int b)
+    {return a+b;
+    }
+}
 
 int main(){
-cout << add(1, 55) << endl;
+cout << special1::sppecial1(1, 55) << endl;
 
 
     return 0;
