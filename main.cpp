f99894d0ae272dc8a310facaff58557a2de12e30
HullBreaker <grochamp5112@gmail.com>
2025-06-05 12:51:51 +0900

    19 헤더파일사용용

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include "add.h"
 using namespace std;
-int a = 0;
-int b = 0;
-void strings(){       //전체 바꾸기 : 우클릭 renamesymbols
-    cout << "문자열" << endl;
-}
+
 
 int main(){
-    strings();
+cout << add(1, 55) << endl;
+
+
+    return 0;
 }
