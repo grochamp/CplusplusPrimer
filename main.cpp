83efebccdeb1d3b281e5732396c533aa50321ee4
HullBreaker <grochamp5112@gmail.com>
2025-06-05 19:11:29 +0900

    34 증감연산자자

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-int main()
-{
+int adding(int a, int b){
+    return a+b;
+}
 
-    int x = 1;
-    int y = x;
-    int z = x - y - 3;
-    int p += y // p = p+y
-    cout << z << endl;
-    cout << -5 % 2 << endl; //앞에 음수로 나머지 구하면 나머지값도 음수다 답 -1
-    
+int main(){
+    int x = 6, y = 6;
+
+    // cout << x << " " << y << endl;
+    // cout << ++x << " " << ++y << endl;  //전처리 7 7 후 출력 : 7 7
+    cout << x++ << " " << y++ << endl; //후처리 : 출력은 그대로 66하고 이후에 7 7 됨
+    cout << x << " " << y << endl; 
+    int a = 4;
+    int b = adding(a, ++a); //쓰지마라. 위험하다. 값은 10이나온다.
+    cout << b << endl;
+
+
     return 0;
 }
