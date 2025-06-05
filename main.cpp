a977cf28ed58be1e45e8cc4db055d4ec1b4a11aa
HullBreaker <grochamp5112@gmail.com>
2025-06-05 09:41:44 +0900

    5.15 포기기

--- main.cpp
+++ main.cpp
@@ -1,21 +1,5 @@
 #include <iostream>
-#include <string>
 using namespace std;
-
-string word;
-char ch;
-int number;
 int main(){
-    cin >> word;
-    for (char ch : word){
-        switch(ch){
-            case 'a': case 'e':
-            ++number;
-            break;
-        }
-    }
-    cout << number << endl;
-
-
-    return 0;
+    cout << "포기" << endl;
 }
