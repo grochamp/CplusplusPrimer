52aece4f7df44e0218be8096633f1287f63a7c74
HullBreaker <grochamp5112@gmail.com>
2025-06-05 14:28:34 +0900

    24 정수형형

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
-
+#include <limits>
+using namespace std;
 int main(){
-
-    using namespace std;
-
-    bool bValue = false;
-    char chValue = 'A'; // 숫자 넣으면 그거에 해당하는 문자 나옴(ex 65넣으면 A나옴)
-    auto aValue = "오토아포칼립스"; // auto : 지가 알아서 판단함
-    cout << chValue << endl;
-    cout << aValue << endl;
-    // cout << (bValue ? 1:0) << endl;
-    cout << sizeof(bool) << endl; //sizeof(x) : size of x
-    int a(123); // direct initialization, 괄호안에는 int안에 소수점수를 넣어도 착해서 소수점만 버리고 인정해준다
-    int b{ 123 }; // uniform initialization 얘는 엄격해서 소수점 넣어도 인정 안해준다
-    int x=128,  y = 256;
-    cout << x << " " << y << endl;
+    short s= 1; //2bytes*8bits = 16bits
+    int i = 1;
+    long l = 1;
+    long long ll= 1;
+    cout << sizeof(short) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(long) << endl;
+    cout << sizeof(long long) << endl;
+    cout << numeric_limits<short>::max() << endl;
+    cout << numeric_limits<short>::min() << endl;
+  
+    s = 32768;
+    --s;
+    cout << s << endl; // 32767까지는 제대로 표현함 근데 short범위를 넘어서서 32768이되면 오버플로우되서 역으로 음의값 -32768이 나옴
+    i = 33;
+    int y = 4;
+    cout << i/y << endl; // int값으로 나누니까 몫만 8로나오고 나머지도 소수점도 안나옴
     return 0;
 }
-
