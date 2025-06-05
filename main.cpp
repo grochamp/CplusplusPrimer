21bae9d7987735c961ea10bc5dfd6beeb78c499a
HullBreaker <grochamp5112@gmail.com>
2025-06-05 19:34:33 +0900

    36 relational operators

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
-using namespace std;
+#include <cmath>
 int main(){
+     using namespace std;
+    
+/*
+     int x, y;
+     cin >> x >> y;
+     cout << "입력한 int 값은 " << x << "와 " << y <<"입니다"<<endl;*/
 
-    float a;
+     double d1(100 - 99.99);
+     double d2(10-9.99);
 
-    cout << sizeof(a) << endl;
-    cout << sizeof(float)<< endl;//size of는 연산자
-
-int x =3;
-int y = 5;
-int z = (++x, ++y);//특이하게 ++x, ++y, ++y 값을 반환함. 괄호를 한 경우만
-cout << x << " " << y << " " << z;
-
-//조건부연산자. 복잡한 환경에선 쓰지마라
-bool onSale = true;
-const int price = (onSale == true)? 10:100;
-
-// if (onSale)
-// price = 10;
-// else
-// price =100;
-cout << price << endl;
+     cout << d1 << endl;
+     cout << d2 << endl;
+     if (d1 == d2){//different
+        cout << "equal" << endl;
+     }
+     else {
+        cout << "not equal" << endl;
+     }
+     cout << d1 - d2 << endl; //5.32907e-15
+    
 
     return 0;
-
 }
