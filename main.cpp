bc4de54fceef0a204e4a0503b902feebca2a69f9
HullBreaker <grochamp5112@gmail.com>
2025-06-05 16:26:29 +0900

    29 문자형형

--- main.cpp
+++ main.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
-int a = 0;
-
-
 int main(){
     using namespace std;
+    char c1(65);//A
+    char c2('A');
+    char c3;
+cout << char(65) << endl;
+cout << int('A') << endl; //65
+
+
+cout << static_cast<char>(65) << endl;//static cast : char로 나오게 강제
+cout << static_cast<int>('A') << endl;//static cast : int로 나오게 강제
+
+// cin >> c3;
+// cout << c3 << " " <<static_cast<int>(c3) << endl;
+
+cout << int('\t') << endl;
+cout << "첫번째라인\n두번째줄";
 
 
 
 
-while(cin >> a){
-    if((a % 2) == 1){
-        cout << "홀수" << endl;
-    }
-    else if(a == 0){
-        cout << 0 << endl;
-    }
-    else{
-        cout << "짝수" << endl;
-    }
-}
-
-
-
-    return 0;
+return 0;
 }
