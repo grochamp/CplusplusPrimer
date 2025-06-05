037756e390ac72199c16233724a1d9868dd8947d
HullBreaker <grochamp5112@gmail.com>
2025-06-05 15:06:23 +0900

    28 불리언 자료형, 조건문if

--- main.cpp
+++ main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
-#include <iomanip>
-#include <limits>
-#include <cmath>
 
-int main(){
-    using namespace std;
+bool isEqual(int a, int b) {
 
-    double zero = 0.0;
-    double posinf = 5.0 / zero;
-    double neginf = -5.0 /zero;
-    double nan = zero / zero;
-    cout << posinf << " " << isnan(nan) << endl;
-    cout << neginf <<  " " << isnan(nan) <<endl;
-    cout << nan << " " << isnan(nan) << endl;
+    bool result = (a == b);
+    return result;
+}
+
+int main(){using namespace std;
+
+    cout << isEqual(5,5) << endl; //true 1
+    cout << isEqual(5,1) << endl; //false 0
+
+
+bool b1 = true ; //copy 
+bool b2(false); //direct
+bool b3{ true }; //uniform
+b3 = false;
+
+cout << (true && true) << endl;//and
+cout << (true || false) << endl; //or
+
+
+// if(true), if(false) : ~일때만 실행됨
+// if(x) x에 0만 아니면 다 True임 0이면 False
+
+
     return 0;
 
 }
