2223cbd1c75bdc4fdb0ba1596937f3c4848a645e
HullBreaker <grochamp5112@gmail.com>
2025-06-06 13:51:08 +0900

    52 조건문

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 int main(){
+/*
+    int x;
+    cin >> x;
 
-    cout << "hello" << endl;
 
-    exit(0);//무조건 종료되어야할경우
+    if(x>10){//하나만 있으면 중괄호 생략도 가능
 
+        cout << "10보다 크다"<< endl;
+    }
+    else if(x<10){
+        cout << "작다" <<endl;
+    }
+    else {
+        cout << "같다" <<endl;
+    }
+*/
+    int a, b;
+    cin >> a >> b;
+    if(a > 0 && b > 0){
+        cout << "크다" << endl;
+    }
+    else if (a==0 && b == 0 ){
+        cout << "영영" << endl;
+    }
+    else if (a > 0 || b > 0 ){//or
+        cout << "하나만" << endl;
+    }
+    else{cout << "뭐지" << endl;}
+
+    return 0;
 }
