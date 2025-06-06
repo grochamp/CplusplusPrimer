8efd0dbf8527ffecc2c19b11f93612ecaab8b624
HullBreaker <grochamp5112@gmail.com>
2025-06-07 00:26:32 +0900

    68 null pointer

--- main.cpp
+++ main.cpp
@@ -1,44 +1,37 @@
 #include <iostream>
-#include <typeinfo>
 using namespace std;
 
-struct Something{
+void doSomething(double *ptr){
+if( ptr != nullptr)
+    {
+
+        //do something useful
+        cout << *ptr << endl;
 
 
-    int a, b, c;
+    }
+    else
+    {
+        //do nothing with ptr(null일 때)
+        cout << "nothing" << endl;
+
+
+    }
+
+    
 };
 
 
 int main(){
 
-    int x = 5;
 
-    int *ptr_x = &x, *ptr_y = &x;
+    double *ptr = nullptr;
 
-    cout << typeid(ptr_x).name() << endl; //pi
-//데이터 타입이랑 주소 상관없음
-    cout << ptr_x << endl;//위치
-    cout <<*ptr_x << endl;//역참조해서 다시 값
-//포인터는 반드시 초기화해서 써라
-
-    Something ss;
-    Something *ptr_s;//Something의 포인터
-    cout << sizeof(Something) << endl;
-    cout << sizeof(ptr_s) << endl;
+    doSomething(ptr);
+    doSomething(nullptr);
+    double d = 3.14;
+    doSomething(&d);
 
 
-
-
-    /*
-    int x = 5; // 변수 값
-    cout << &x << endl; // x의 메모리 주소
-
-    //de-reference operator(*) :직접접근
-
-    cout << *&x << endl;
-
-
-*/
-
-    return 0;
-}
+    
+return 0;}
