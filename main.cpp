52d622d894877a873f8a24fd980b9023dc8440c9
HullBreaker <grochamp5112@gmail.com>
2025-06-06 10:11:53 +0900

    49 자료형 이름붙이기기

--- main.cpp
+++ main.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
-#include <typeinfo>
+#include <vector>
+
 using namespace std;
 
-enum class Color //enum에 class를 더해서 영역제한
-{
-    COLOR_BLACK, 
-    COLOR_RED,
-    COLOR_BLUE,
-    COLOR_GREEN,
-};
-
-enum class Food
-{
-    RAMEN,
-    HAMBURGER,
-    RICE,
-    SUSHI,
-
-};
 
 
 int main(){
-    Color color = Color::RED;
-    Food food = Food::RAMEN;
-
-    
 
 
-  
+    typedef vector<pair<string, int> > pairlist_t;
+    using pairlist_t = vector<pair<string, int> >; 
+    typedef double distance_t;//자료형에게 이름 붙이기기
+    pairlist_t pairlist1;
+
 
     return 0;
 }
