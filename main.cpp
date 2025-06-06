a4533a83b6ed68191fe402deb8fff3d4cdf0de53
HullBreaker <grochamp5112@gmail.com>
2025-06-06 15:07:30 +0900

    57 for 반복문문

--- main.cpp
+++ main.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 using namespace std;
 
+
+int pow(int base, int exponent){
+
+    int result = 1;
+
+    for (int count = 0; count < exponent; ++count)
+    result *= base;
+    return result;
+}
+
 int main(){
 
 
-    int count = 10;
-    // while(1){
 
 
-    //     cout << count << endl;
-    //     ++count;
+int count = 0;
+for(; count < 10; ++count)//값을 밖에두고 그냥 ;로 부를수있다. 조건도 없애고 그냥 ;;로 할수도있다.
+{
+ 
+cout << count << endl;
+cout << pow(2,4) << endl;
 
-    //     if(count == 10) break;
-
-    while(1){
-
-        // cout << count << endl;
-        ++count;
-        if (count  % 1000 == 0) {
-        cout << "1000의 배수" << " " << count << endl;}
-        
-        // break;
-   
+}
 
 
-    }
 
     return 0;
-    //do while : 일단 무조건 실행함. 이후에 확인함. dowhile에 ; 찍어야함
 }
