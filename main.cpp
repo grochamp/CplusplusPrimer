8b287ddc9019da6450bb4368ebce736c5f9209c2
HullBreaker <grochamp5112@gmail.com>
2025-06-06 16:36:46 +0900

    62 array

--- main.cpp
+++ main.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
 using namespace std;
-
-void breakorReturn(){
-    while (true)
-    {
-        char ch;
-        cin >> ch;
-
-        if (ch == 'b')
-        break;
-        if(ch == 'r')
-        return;
-//continue : 아래에 있는거 실행안하고 다음값으로감
-    }
-cout << "hello"<< endl;
-
-}
-
 int main(){
 
-    breakorReturn();
+
+    const int num_students = 20;
+int student_scores[num_students] = {99, 76, 35, 77, 66,}; //배열은 그자체로 주소나타냄
+cout << &student_scores << endl;
+cout << &student_scores[0] << endl;
+cout << &student_scores[1] << endl;//int는 변수 하나가 4바이트라 4차이남
+
+
+
 
 
     return 0;
-
 }
