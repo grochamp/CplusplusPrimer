218cee4970bedac6708f7ab4a399dcabd0a68efc
HullBreaker <grochamp5112@gmail.com>
2025-06-06 17:17:25 +0900

    63 배열과 반복문문

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
+
 int main(){
 
+    const int num_students = 5;
+    int scores[] = {74, 66, 12, 78, 90};
+    int total_score = 0;
+    int max_score = 0;
 
-    const int num_students = 20;
-int student_scores[num_students] = {99, 76, 35, 77, 66,}; //배열은 그자체로 주소나타냄
-cout << &student_scores << endl;
-cout << &student_scores[0] << endl;
-cout << &student_scores[1] << endl;//int는 변수 하나가 4바이트라 4차이남
 
+    for (int i = 0; i < num_students; ++i){
+
+        total_score +=scores[i];
+        if (max_score<scores[i])
+        {
+            max_score=scores[i];
+        }
+        
+    }
+    cout << total_score << endl;
+    cout << max_score << endl;
+    
 
 
 
