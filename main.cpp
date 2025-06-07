37b2e141c71d7224ce466d65aaa6156c857fb111
HullBreaker <grochamp5112@gmail.com>
2025-06-07 17:32:15 +0900

    77 포인터의 참조조

--- main.cpp
+++ main.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
 using namespace std;
 
+struct Person{
 
-void doSomething(const int& x){
+    int age;
+    double weight;
+    string name;
 
-    cout << x << endl;
-}
+};
+
+
+
 int main(){
 
-    int a =1;
-    doSomething(a);
-    doSomething(1);
-doSomething(1+a);
-    // const int x =5;
-    // const int &ref_x = x;
-    // const int &ref_2 = ref_x;//const ref에는 literal 입력 가능
+
+    Person person;
+    person.age= 5;
+    person.weight=30;
+    Person &ref = person;
+    ref.age = 15;
+
+
+    Person *ptr = &person;
+    ptr->age =30; //표현방법이 이래요
+    (*ptr).age = 20;
+
+    Person &ref2 = *ptr;
+    ref.age = 43;
+    cout << &person << endl;
+    cout << &ref2 << endl;
+
+
+
+
+
+
     return 0;
 }
