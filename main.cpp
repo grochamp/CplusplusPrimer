172624c755ea9f8af686d9dad7738159c9083da9
HullBreaker <grochamp5112@gmail.com>
2025-06-07 17:27:38 +0900

    76 참조와 const

--- main.cpp
+++ main.cpp
@@ -2,54 +2,18 @@
 using namespace std;
 
 
-struct Something
-{
+void doSomething(const int& x){
 
-    int v1;
-    float v2;
-};
-
-struct Other
-{
-
-    Something st;
-};
-
-
-
-
-
-
-void doSomething(const int &n)
-{
-    cout << &n << endl;
-    //n =10;//const 때문에 못바꿈
-    cout << "In doSomething" << " " <<  n << endl;
+    cout << x << endl;
 }
-
-
-
-
 int main(){
-    Other ot;
-    int &v1 = ot.st.v1;
-    v1 = 1;
-    int value = 5;
-    int *const ptr = &value;
-    int &ref =value;
-    
-    
-    // int n = 5;
 
-    // cout << n << endl;
-    // cout << &n << endl;
-
-    // doSomething(n);
-
-    // cout << n << endl;
-
-
-    // return 0;
-
-
+    int a =1;
+    doSomething(a);
+    doSomething(1);
+doSomething(1+a);
+    // const int x =5;
+    // const int &ref_x = x;
+    // const int &ref_2 = ref_x;//const ref에는 literal 입력 가능
+    return 0;
 }
