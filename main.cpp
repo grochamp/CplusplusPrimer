37fe77dfeee6cfe8e2337222c5b117b1363b7df2
HullBreaker <grochamp5112@gmail.com>
2025-06-07 15:12:27 +0900

    73 동적할당배열열

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 using namespace std;
+
 int main(){
 
-    while (true)
-    { int *ptr = new int;
-        cout << ptr << endl;
-        delete ptr;
-        /* code */
-    }
-    
 
+    int length;
+    cin >> length;
+
+    int *array = new int[length]{17, 12, 66, 231};//array[] 0~3위치까지 얘네가 차지함
+
+    array[0] = 1;
+    array[1] = 2;
+    for(int i = 0; i < length; ++i)
+    {
+
+        cout << (uintptr_t)&array[i] << endl;
+        cout << array[i] << endl;
+    }
 
 
     return 0;
