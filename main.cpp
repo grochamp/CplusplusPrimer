71d09c04f14d9d5c9d69317941df65ca6ab0ee97
HullBreaker <grochamp5112@gmail.com>
2025-06-07 18:48:25 +0900

    79 void pointer

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 using namespace std;
+
+
+
 int main(){
 
+    int i =5;
+    float f = 3.2;
+    char c = 'a';
 
-    int fibonacci[] = {0, 1, 1, 2, 3, 5, 8, 13, 21,34, 55, 89};
+    void *ptr = nullptr;
 
+    ptr = &i;
+    ptr = &f;
+    ptr = &c;
 
-    for (auto &number : fibonacci)
-    number *=10;
+cout << &f << " " << &i << endl;
+cout << static_cast<float*>(ptr) << endl;//구차하게해야함 실제로 해야하는경우도있음
 
-for (const auto &number :fibonacci)
-cout << number << " ";
-cout << endl;
-
-
-
-
-
-
-return 0;
-
+    return 0;
 }
