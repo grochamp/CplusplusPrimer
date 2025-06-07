015584e61f6de516f75d2f9a4279f99fa04aa4d0
HullBreaker <grochamp5112@gmail.com>
2025-06-07 21:21:10 +0900

    85 참조에 의한 인수 전달달

--- main.cpp
+++ main.cpp
@@ -1,19 +1,68 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-void doSomething(int y)
+void addOne(int &y)//참조에 의한 전달 : 주소 그대로임
 {
-    cout << "In func" << y << " " << &y << endl;
+    y = y + 1;
+}
+
+
+
+
+
+void getSinCos(double degrees, double &sin_out, double &cos_out)
+{
+
+    static double pi = 3.141592;
+    double radians = degrees * pi / 180.0;
+    sin_out = sin(radians);
+    cos_out = cos(radians);
+}
+
+
+void foo(int (&arr)[4])
+{
+    cout << arr << endl;
+    cout << &arr << endl;
+
+}
+
+void printElement(int(&arr)[4])//반드시 대괄호 갯수 나와야함
+{
+
+
+
 }
 
 int main()
 {
-    doSomething(5);
-    int x = 6;
-    cout << "In main" << x << " " << &x << endl;
+    int arr[]{1, 2, 3, 4};
+    printElement(arr);
+    
+    // double sin(0.0);
+    // double cos(0.0);
 
-    doSomething(x);
-    doSomething(x+1);
+    // getSinCos(30.0, sin, cos);
+
+    // cout << sin << " " << cos << endl;
+
+
+    // int x =5 ;
+    // int *ptr = &x;
+
+    // cout << x << " " << &x << endl;
+
+    // addOne(x);
+
+    // cout << "변한 값 " <<  x << " " << &x << endl;
+
+    // foo(6);
+
+
+
+
+
 
     return 0;
 }
