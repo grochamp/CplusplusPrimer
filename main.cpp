da04ba2ceee87e7f11b7c80523f841816b5d7f51
HullBreaker <grochamp5112@gmail.com>
2025-06-07 14:47:29 +0900

    71 c style char

--- main.cpp
+++ main.cpp
@@ -1,37 +1,31 @@
 #include <iostream>
 using namespace std;
+
+
+const char* getName()
+{
+
+    return "Jokichi";
+}
+
+
+
 int main(){
 
+    const char *name = getName();
+    const char *name2 = getName();
     
-    char name [] = "LeBron James";
-    const int n_name = sizeof(name) / sizeof(char);
 
-    for (int i = 0; i < n_name; ++i)
-    {
-        cout << *(name+i);
+    cout << (uintptr_t)name << endl;
+    cout << (uintptr_t)name2 << endl;//내용이 같으면 주소도 같음
 
-    }
-    cout << endl;
+    char d = 'P';
+    cout << &d << endl;//이상한값나옴:널찾느라
 
 
 
 
 
-
-
-    double array[] = { 9, 5, 1, 7};
-
-    double *ptr = array;
-    for (int i = 0; i < 5; ++i)
-    //cout << array[i] << " " << (uintptr_t)&array[i] << endl;//double array 라서 8씩 늘음
-    cout << *(ptr + i) << " " << uintptr_t(ptr+i) << endl;
-    
-/*
-    int value = 7;
-    int *ptr = &value;
-    cout << uintptr_t(ptr) << endl;
-    cout << uintptr_t(ptr+1) << endl;//int 1 늘어나서 4늘어남
-*/
 
     return 0;
 }
