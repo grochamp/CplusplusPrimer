6748eefd74444b7f8b7fcb4c1922a4acea82a528
HullBreaker <grochamp5112@gmail.com>
2025-06-07 20:07:11 +0900

    81 std array

--- main.cpp
+++ main.cpp
@@ -1,58 +1,27 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
-
-int main(){
-
-    const int row = 3;
-    const int col = 5;
-
-    const int s2da[row][col] = {
-
-        {1,2,3,4,5},
-        {6,7,8,9,10},
-        {11,12,13,14,15}
+void printLength(const array<int, 5>& my_arr)
+{
+cout << my_arr() << endl;
 
 
 
-    };
 
-    int **matrix = new int*[row];
-    for(int r = 0; r<row;++r)
-    {
-        matrix[r] = new int[col];
-    }
-    
+}
 
-    // int *r1 = new int[col] {1,2,3,4,5};
-    // int *r2 = new int[col] {6,7,8,9,10};
-    // int *r3 = new int[col] {11,12,13,14,15};
+int main()
+{
 
-    // int **rows = new int*[row] {r1, r2, r3};
+array<int, 5> my_arr = {1, 2, 3, 4, 5};
+my_arr = {0,1,2,3,4};
+my_arr = {0,1,2};
 
-    for(int r = 0; r < row; ++r)
-    {
-        for(int c = 0; c < col; ++c)
-        cout << rows[r][c] << " ";
-        cout << endl;
+cout << my_arr[10] << endl;
+cout << my_arr.at(10) << endl;
 
 
-    }
+return 0;
 
-    delete[]r1;
-    delete[]r2;
-
-    // int *ptr = nullptr;
-    // int **ptrptr = nullptr;
-
-    // int value = 5;
-
-    // ptr = &value;
-    // ptrptr = &ptr;
-    // cout << ptr << " " << *ptr << " " << &ptr << endl;
-    // cout << ptrptr << " " << *ptrptr << " " << &ptrptr << endl;
-    // cout << **ptrptr << endl;
-
-
-    return 0;
 }
