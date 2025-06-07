d93d6edb858ae000b28d19482ff7533934a554ba
HullBreaker <grochamp5112@gmail.com>
2025-06-07 15:16:30 +0900

    73 동적 할당 배열 addition

--- main.cpp
+++ main.cpp
@@ -3,21 +3,12 @@ using namespace std;
 
 int main(){
 
+    int fixedArray[] = { 1, 3, 5, 7};
 
-    int length;
-    cin >> length;
+    int *array = new int[6] {1, 2, 3 ,4, 5};
 
-    int *array = new int[length]{17, 12, 66, 231};//array[] 0~3위치까지 얘네가 차지함
 
-    array[0] = 1;
-    array[1] = 2;
-    for(int i = 0; i < length; ++i)
-    {
-
-        cout << (uintptr_t)&array[i] << endl;
-        cout << array[i] << endl;
-    }
-
+    delete[] array;
 
     return 0;
 }
