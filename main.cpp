ee87053b4369d4d8376dbbbf99e971cdcfc2fead
HullBreaker <grochamp5112@gmail.com>
2025-06-07 19:59:25 +0900

    80 multipe pointer

--- main.cpp
+++ main.cpp
@@ -2,21 +2,57 @@
 using namespace std;
 
 
-
 int main(){
 
-    int i =5;
-    float f = 3.2;
-    char c = 'a';
+    const int row = 3;
+    const int col = 5;
 
-    void *ptr = nullptr;
+    const int s2da[row][col] = {
 
-    ptr = &i;
-    ptr = &f;
-    ptr = &c;
+        {1,2,3,4,5},
+        {6,7,8,9,10},
+        {11,12,13,14,15}
 
-cout << &f << " " << &i << endl;
-cout << static_cast<float*>(ptr) << endl;//구차하게해야함 실제로 해야하는경우도있음
+
+
+    };
+
+    int **matrix = new int*[row];
+    for(int r = 0; r<row;++r)
+    {
+        matrix[r] = new int[col];
+    }
+    
+
+    // int *r1 = new int[col] {1,2,3,4,5};
+    // int *r2 = new int[col] {6,7,8,9,10};
+    // int *r3 = new int[col] {11,12,13,14,15};
+
+    // int **rows = new int*[row] {r1, r2, r3};
+
+    for(int r = 0; r < row; ++r)
+    {
+        for(int c = 0; c < col; ++c)
+        cout << rows[r][c] << " ";
+        cout << endl;
+
+
+    }
+
+    delete[]r1;
+    delete[]r2;
+
+    // int *ptr = nullptr;
+    // int **ptrptr = nullptr;
+
+    // int value = 5;
+
+    // ptr = &value;
+    // ptrptr = &ptr;
+    // cout << ptr << " " << *ptr << " " << &ptr << endl;
+    // cout << ptrptr << " " << *ptrptr << " " << &ptrptr << endl;
+    // cout << **ptrptr << endl;
+
 
     return 0;
 }
