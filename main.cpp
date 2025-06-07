9293cea9fe6bb6459a89e86cb8a861418231099f
HullBreaker <grochamp5112@gmail.com>
2025-06-07 23:54:10 +0900

    90 매개변수 기본값값

--- main.cpp
+++ main.cpp
@@ -2,28 +2,21 @@
 #include <string>
 using namespace std;
 
+void print(string str){}
+void print(char ch =' '){}
 
-typedef int my_int;
-
-void print(char *value){}//애매한 경우 오버로딩 안됨
-
-//void print (int x) {}
-//void print (my_int x){}//어차피 얘는 int라서 오버로딩으로 둘이 같은취급함
-/*
-int add(int x, int y)//들어오는 수 똑같은데 int, double같이 약간 타입 다를경우 사용가능
+//void print(int x = 99)//함수에 아무값도 안넣으면 default 99나옴. 오른쪽 값에 default없으면 왼쪽에 default값 넣을 수 없다
 {
-    return x + y;
+    cout << x << endl;
 
 }
 
-double add(double x, double y)
-{
 
-    return x + y;
-}
-*/
+
+
 int main()
 {
-    
+    print(10);
+    print();
     return 0;
 }
