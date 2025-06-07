0b0699b902f0489cf82b2b06abd37745ef63514f
HullBreaker <grochamp5112@gmail.com>
2025-06-07 22:21:20 +0900

    86 주소에 의한 인수 전달달

--- main.cpp
+++ main.cpp
@@ -1,68 +1,22 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-void addOne(int &y)//참조에 의한 전달 : 주소 그대로임
+void foo(int *ptr)
 {
-    y = y + 1;
+
+    cout << *ptr << " " << ptr << " " << &ptr << endl;
 }
+int main(){
 
+    int value = 5;
 
+    cout << value << " " << &value << endl;
 
+    int *ptr = &value;
 
-
-void getSinCos(double degrees, double &sin_out, double &cos_out)
-{
-
-    static double pi = 3.141592;
-    double radians = degrees * pi / 180.0;
-    sin_out = sin(radians);
-    cos_out = cos(radians);
-}
-
-
-void foo(int (&arr)[4])
-{
-    cout << arr << endl;
-    cout << &arr << endl;
-
-}
-
-void printElement(int(&arr)[4])//반드시 대괄호 갯수 나와야함
-{
-
-
-
-}
-
-int main()
-{
-    int arr[]{1, 2, 3, 4};
-    printElement(arr);
-    
-    // double sin(0.0);
-    // double cos(0.0);
-
-    // getSinCos(30.0, sin, cos);
-
-    // cout << sin << " " << cos << endl;
-
-
-    // int x =5 ;
-    // int *ptr = &x;
-
-    // cout << x << " " << &x << endl;
-
-    // addOne(x);
-
-    // cout << "변한 값 " <<  x << " " << &x << endl;
-
-    // foo(6);
-
-
-
-
-
+    foo(ptr);
+    foo(&value);
 
     return 0;
+
 }
