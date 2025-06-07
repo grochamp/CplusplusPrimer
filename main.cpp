e750162d4624c86fb586c3eb484b04150c2df83b
HullBreaker <grochamp5112@gmail.com>
2025-06-07 09:11:46 +0900

    69 pointer와 정적배열열

--- main.cpp
+++ main.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 using namespace std;
 
-void doSomething(double *ptr){
-if( ptr != nullptr)
-    {
 
-        //do something useful
-        cout << *ptr << endl;
+void printArray(int array[]){
 
+    cout << sizeof(array) << endl; //배열 포인터값임 배열 크기가 아니고
+    cout << *array << endl;
 
-    }
-    else
-    {
-        //do nothing with ptr(null일 때)
-        cout << "nothing" << endl;
-
-
-    }
-
-    
-};
-
-
+    *array = 100;
+}
 int main(){
 
+    int array[5] = {9, 7, 5, 3, 1};
 
-    double *ptr = nullptr;
+    cout << sizeof(array) << endl;
 
-    doSomething(ptr);
-    doSomething(nullptr);
-    double d = 3.14;
-    doSomething(&d);
+    int *ptr = array;
+    cout << sizeof(ptr) << endl;
 
-
+    cout << &array[0] << endl;
+    cout << *array << endl;
     
-return 0;}
+    // int *ptr = array;
+    // cout << ptr << endl;
+    // cout << *ptr << endl;
+
+
+    char name[] = "jackjack";
+    cout << *name << endl;
+
+    printArray(array);
+    cout << array[0] << " " << *array << endl;
+    return 0;
+}
