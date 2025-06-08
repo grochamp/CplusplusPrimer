b9c3cf49116bbdf643a3e7da95fbbbd0564fef5a
HullBreaker <grochamp5112@gmail.com>
2025-06-08 13:41:27 +0900

    100캡슐화접근함수수

--- main.cpp
+++ main.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 using namespace std;
 
-class Friend
+
+class Date
 {
+//public:
+    int m_month;
+    int m_day;
+    int m_year;
+
 public:
-    string name;
-    string address;
-    int age;
-    double height;
-    double weight;
+void setDate(const int& month_input, const int& day_input, const int& year_input)
+{
+    m_month = month_input;
+    m_day = day_input;
+    m_year = year_input;
+
+}
+int getDay()
+{
+    return m_day;
+}
+
 
 };
 
-int main(){
+int main()
+{
+    Date today;
+    today.setDate(8, 4, 2025);
+    cout << today.getDay() << endl;
 
+
+    
     return 0;
 }
