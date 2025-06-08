5226c7a947f5035ec20472fa1081f83e6fceebc9
HullBreaker <grochamp5112@gmail.com>
2025-06-08 10:21:13 +0900

    96 assert

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <cassert>
+#include <array>
 using namespace std;
 
-void fibonachi(int count, int cont)
+void printValue(array<int, 5> &my_array, const int& ix)
 {
+assert(ix >=0);
+    assert(ix <= my_array.size() -1);
 
-    cout << count+ cont  << endl;
-    if((count + cont)>100) return;
-    fibonachi(cont, count+cont);
-
+    cout << my_array[ix] << endl;
 
 }
 
 int main()
-    {
+{
+    const int z = 11;
+    assert(z == 5);
+    static_assert(z == 5, "x must 5");
 
-        fibonachi(0, 1);
-        return 0;
-    }
+
+
+    array<int, 5> my_array{1, 2, 3, 4, 5};
+    printValue(my_array, 100);
+    //assert(false);//디버그 모드에서만 발동
+
+
+    return 0;
+}
