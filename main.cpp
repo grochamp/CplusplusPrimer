b78f9f866addcd9b0d285c5f909533f2e1bd5102
HullBreaker <grochamp5112@gmail.com>
2025-06-08 17:22:15 +0900

    110  친구함수 뭐라는지 모르겠다다

--- main.cpp
+++ main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 using namespace std;
 
-class Something
+
+class B
+
+
+class A
 {
-//스태틱은 이니셜라이즈 불가
-    
-    public:
-    static int getValue()
-    {
-        return s_value;
-    }
-    int temp()
-    {
-        return this ->s_value;
-    }
+    private:
+    int m_value = 1;
+
+    friend void B::doSomething(A& a);
 };
 
-int Something::s_value = 1024;
-//int Something::s_value = 1;//정적이라서 접근가능
+class B
+{
+
+    private:
+    int m_value = 2;
+    public:
+   void doSomething(A& a,);
+   {
+    cout << a.m_value << endl;
+   }
+}
+
+
+
 int main()
 {
-    cout << Something::getValue() << endl;
-    Something s1;
-   
-    cout << s1.getValue() << endl;
-    //cout << &s1.s_value << endl;
-    int(Something::*fptr1)()== &Something::temp;
-    cout << (s2.*fptr1)() << endl;
+    A a;
+    doSomething(a);
 
     return 0;
 }
