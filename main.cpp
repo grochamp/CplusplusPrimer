22831457ac976907c3442fe587e77425e70429d4
HullBreaker <grochamp5112@gmail.com>
2025-06-08 13:58:51 +0900

    101 생성자자

--- main.cpp
+++ main.cpp
@@ -1,37 +1,34 @@
 #include <iostream>
 using namespace std;
 
-
-class Date
+class Fraction
 {
-//public:
-    int m_month;
-    int m_day;
-    int m_year;
+private:
+    int m_numerator; //값 지정 가능
+    int m_denominator;
+
 
 public:
-void setDate(const int& month_input, const int& day_input, const int& year_input)
-{
-    m_month = month_input;
-    m_day = day_input;
-    m_year = year_input;
 
-}
-int getDay()
-{
-    return m_day;
-}
+    Fraction(const int& num_in, const int& den_in = 1)
+    {
 
-
+        m_numerator = num_in;
+        m_denominator = den_in;
+    }
+    void print()
+    {
+        cout << m_numerator << " / " << m_denominator << endl;
+    }
 };
 
 int main()
 {
-    Date today;
-    today.setDate(8, 4, 2025);
-    cout << today.getDay() << endl;
+    //Fraction frac;//파라미터가 없을때는 괄호 빼기
+    //frac.print();
+    Fraction one_thirds(1, 1);
+    one_thirds.print();
 
 
-    
     return 0;
 }
