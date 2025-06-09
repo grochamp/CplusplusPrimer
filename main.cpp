7a9d6e8f13c224ec3fb976ae7b46bda819176b57
HullBreaker <grochamp5112@gmail.com>
2025-06-10 01:51:00 +0900

    142 virtual

--- main.cpp
+++ main.cpp
@@ -1,80 +1,49 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-class Animal
-{
-    protected :
-        string m_name;
-
-    public :
-        Animal(string name)
-            : m_name(name)
-            {}
-
-    public :
-        string getName() {return m_name; }
-
-        virtual void speak() const//virtual : 자식을 따라가게해줌
-        {
-            cout << m_name << " ???" << endl;
-        }
-};
-
-class Cat : public Animal
+class A
 {
     public:
-        Cat(string name)   
-            : Animal(name)//Animal에서 이름 받아서 Cat에 씀
-            {}
+        virtual void print() {cout << "A" << endl;} //virtual = 느리다.
+};
 
-        void speak() const
-        {
-            cout << m_name << " Meow" << endl;
-        }
+class B : public A
+{
+    public :
+        void print() {cout << "B" << endl;}
 
 };
 
-class Dog : public Animal
+class C : public B
 {
         public:
-            Dog(string name)
-                : Animal(name)
-                {}
+        void print() {cout << "C" << endl;}
 
-        void speak() const
-        {
-            cout << m_name << " Woof" << endl;
-        }
+};
 
+class D : public C
+{
+        public:
+        void print() {cout << "D" << endl;}
 
 };
 
 int main()
 {
-    Animal animal("my animal");
-    Dog dog("my dog");
-    Cat cat("my cat");
+    A a;
+    a.print();
 
+    B b;
+    b.print();
 
-    animal.speak();
-    dog.speak();
-    cat.speak();
+    C c;
+    c.print();
 
-    Animal *ptr_animal1 = &cat;  //포인터로 호출하면 자기가 부모(Animal)인줄 알고 행동한다
-    Animal *ptr_animal2 = &dog;
+    D d;
+    d.print();
 
-    ptr_animal1->speak();
-    ptr_animal2->speak();
+    B *ptr= &c;
+    ptr->print();
 
-    Cat cats[] = { Cat("cat1"),Cat("cat2"),Cat("cat3"),Cat("cat4"),Cat("cat5"),Cat("cat6")};
-    Dog dogs[] = { Dog("dog1"),Dog("dog2")};
-
-    Animal *my_animals[] = {&cats[0], &cats[1], &cats[2], &cats[3], 
-                                &dogs[0], &dogs[1]};
-
-    for (int i = 0; i <6; ++i)
-        my_animals[i]->speak();
     return 0;
-
 }
