e406cb0a08b5c31fdf4ea81c94743060a842445d
HullBreaker <grochamp5112@gmail.com>
2025-06-09 11:10:23 +0900

    127 객체 organize relationship

--- main.cpp
+++ main.cpp
@@ -1,60 +1,16 @@
 #include <iostream>
-#include <cassert>
-#include <initializer_list>
+#include "monster.h"
+
 using namespace std;
-
-class IntArray
-{
-    private:
-        unsigned m_length = 0;
-        int *m_data = nullptr;//어떤 메모리도 가리키지 않는다
-
-
-    public:
-        IntArray(unsigned length)
-            :m_length(length)
-            {
-                m_data = new int[length];
-            }    
-
-
-IntArray(const initializer_list<int> &list)
-    : IntArray(list.size())
-
-{
-        int count = 0;
-        for (auto & element : list)
-        {
-
-            m_data[count] = element;
-            ++count;
-        }
-}
-~IntArray()
-{
-    delete[] this->m_data;
-}
-
-friend ostream & operator << (ostream & out, IntArray & arr)
-{
-    for(unsigned i = 0; i < arr.m_length; ++i)
-        out << arr.m_data[i] << " ";
-        out << endl;
-        return out;
-
-
-}
-};
-
 
 int main()
 {
-    int my_arr1[5] = {1, 2, 3, 4, 5};//정적 할당
-    int *my_arr2 = new int[5]{1, 2, 3, 4, 5};//동적 할당
-    auto il = { 10, 20, 30};
 
-    IntArray int_array {1, 2, 3, 4, 5};
-    cout << int_array << endl;
+    Monster mon1("abrahm", Position2D(0, 0));
+    {
+            mon1.moveTo(Position2D(1, 1));
+            cout << mon1 << endl;
 
+    }
     return 0;
 }
