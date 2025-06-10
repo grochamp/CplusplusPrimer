25ef2449b2f2db8fc2934930d423951daf4ef4c0
HullBreaker <grochamp5112@gmail.com>
2025-06-10 19:42:42 +0900

    156 class template specialization

--- main.cpp
+++ main.cpp
@@ -1,14 +1,42 @@
-#include "Storage.h"
+#include <iostream>
+#include <array>
+//#include "Storage8.h"
+
+using namespace std;
+
+template<typename T>
+class A
+{
+    public:
+         void doSomething()
+         {
+            cout << typeid(T).name() << endl; //매개변수 T의 타입 이름 출력
+         }
+
+
+         void test()
+         {}
+};
+
+template<>
+class A<char>
+{
+    public :
+    void doSomething()
+    {
+        cout << "ITISCHARSPECIALIZATION" << endl;
+    }
+};
+
+
 
 int main()
 {
-    Storage<int> nValue(5);
-    Storage<double> dValue(6.6);
+    A<int> a_int;
+    A<double> a_double;
+    A<char> a_char;
 
-    nValue.print();
-    dValue.print();
-
-    return 0;
-
-
+    a_int.doSomething();
+    a_double.doSomething();
+    a_char.doSomething();
 }
