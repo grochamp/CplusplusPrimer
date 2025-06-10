d903e91aeb4a6d3ea8ed6a9ce67a73b2e95dd05e
HullBreaker <grochamp5112@gmail.com>
2025-06-10 17:44:12 +0900

    153 class template

--- main.cpp
+++ main.cpp
@@ -1,16 +1,71 @@
+#pragma once
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
 template<typename T>
-T getMax(T x, T y)
+class MyArray
 {
-    return  (x>y) ? x : y;
-}
+    private:
+        int m_length;
+        T *m_data;
+
+    public :
+        MyArray()
+        {
+
+            m_length = 0;
+            m_data = nullptr;
+        }
+        MyArray(int length)
+        {
+
+            m_data = new T[length]; // 동적 배열은 반드시 delete[] 로 해제 해야함
+            m_length = length;
+        }
+
+        ~MyArray()
+        {
+            reset();
+        }
+
+        void reset()
+        {
+            delete[] m_data;
+            m_data = nullptr;
+            m_length = 0;
+        }
+
+        T & operator[](int index)
+        {
+            assert(index >= 0 && index < m_length);
+            return m_data[index];
+        }
+
+        int getLength()
+        {
+            return m_length;
+        }
+
+        void print()
+        {
+            for (int i = 0; i < m_length; ++i)
+                cout << m_data[i] << " ";
+                cout << endl;
+        }
+
+
+    };
 
 int main()
 {
-    cout << getMax(1, 7) << endl;
-    cout << getMax(3.14, 8.88) << endl;
-    cout << getMax("a", "c") << endl;
+    MyArray<double> my_array(10);
+    for (int i = 0; i < my_array.getLength(); ++i)
+        my_array[i]= i * 10;
+
+    my_array.print();
+
     return 0;
+
+
 }
