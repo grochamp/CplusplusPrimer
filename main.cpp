9639fe76691d5efd8004121b221cd94a1ecb1db3
HullBreaker <grochamp5112@gmail.com>
2025-06-11 13:05:57 +0900

    163 exception

--- main.cpp
+++ main.cpp
@@ -1,46 +1,34 @@
-//예외 클래스 만들기
 #include <iostream>
+#include <exception>
+#include <string>
 using namespace std;
 
-class Exception
-{
-    public:
-    void report()//
-    {
-        cerr << "Exception report" << endl; //cerr : cout랑 비슷
-    }
+//std exception
 
+class CustomException : public exception
+{
+    public :
+        const char * what() const noexcept
+        {
+            return "Custom excpetion";
+        }
 };
 
-class MyArray;
-{
-    private://타지역에서 참고불가 friend로 상속에서 참고가능
-        int m_data[5]//int형 m_data 배열(인자수 6개)
-  public :
-  int & operator[] (const int & index) //배열 오퍼레이터 ex) array[5] = 0 가능
 
-    if (index < 0 || index >= 5) throw Exception(); //음수번째, 6번째 넘어가면 -1로 예외처리 
+int main()
 {
-    return = m_data[index];
-
-}
-};
-
-void doSomething()
-{
-    MyArray my_array;
 
     try
     {
-        my_array[100];
-    }
-    catch (const int & x)
-    {
-        cerr << "Exception" << x << endl;
+
+        //string s;
+        //s.resize(-1);
+        throw CustomException();
     }
 
-    catch (Exception & e)
-    {
-        e.report();
-    }
+catch(exception & exception)
+{
+    cout << typeid(exception).name() << endl;
+    cerr << exception.what() << endl;//뭐가 문제인지 알려줌
+}
 }
